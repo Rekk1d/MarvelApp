51fa9e46d9083f203ca665f0a147f509610dad1f
Misha <xxxdaddy420@gmail.com>
2022-03-30 13:49:02 +0700

    create updateChar function

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import App from './components/app/App';
 import MarvelService from './services/MarvelService.js';
 import './style/style.scss';
 
-const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -10,11 +10,23 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformChar())
     }
-    getCharacter = (id) => {
-        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformChar(res.data.results[0])
+    }
+
+    _transformChar = (char) => {
+        return  {
+            name: char.name,
+            description: char.description ? char.description.slice(0, 210) + '...' : 'No information' ,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
