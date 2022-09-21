3c1e9e1229907e3a9f9a00f22ea976ffc6c566e3
Misha <xxxdaddy420@gmail.com>
2022-09-21 12:59:25 +0700

    changed max lenght comics in charInfo component, changed style thumbnail

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -69,7 +69,7 @@ const CharInfo = (props) => {
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char
     let imgStyle = {'objectFit' : 'cover'};
-    if(thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+    if(thumbnail.includes('image_not_available')) {
         imgStyle = {'objectFit' : 'contain'}
     }
     return (
@@ -95,10 +95,7 @@ const View = ({char}) => {
         <ul className="char__comics-list">
             {comics.length > 0 ? null : 'No comics :('}
             {
-                comics.map((item, i) => {
-                    if(item > 9) {
-                        return
-                    }
+                comics.slice(0,10).map((item, i) => {
                     return (
                         <li className="char__comics-item" key={i}>
                             {item.name}
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -78,7 +78,7 @@ const RandomChar = () => {
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
     let imgStyle = {'objectFit' : 'cover'};
-    if(thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+    if(thumbnail.includes('image_not_available')) {
         imgStyle = {'objectFit' : 'contain'}
     }
     return (
